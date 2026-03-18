54ac9e7120a9bbbd49d3dadd16053892b743cbd4
IgorVinicius01 <igor.domingos@dcx.ufpb.br>
2026-03-18 02:31:40 -0300

    feat(home): implementação interface principal de avisos

--- FrontEnd/src/components/Header.tsx
+++ FrontEnd/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
@@ -10,31 +11,75 @@ function Header() {
                 justify-between
                 items-center
                 px-4
-                bg-blue-400"
+                bg-blue-400
+                sm:px-10"
         >
             <h1
                 className="
                 text-3xl
                 font-bold
-                text-white"  
+                text-white
+                sm:text-5xl"  
             >
-                Mural
+                Mural de Avisos
             </h1>
 
             <Sheet>
                 <SheetTrigger asChild>
-                <Button className="p-3 cursor-pointer"> 
+                <Button className="p-3 cursor-pointer sm:p-5"> 
                     <MenuIcon size={24}/> 
                 </Button>
                 </SheetTrigger>
 
                 <SheetContent side="top">
-                <SheetHeader>
-                    <SheetTitle>Navegação do Mural</SheetTitle>
-                    <SheetDescription>Selecione uma das opções abaixo</SheetDescription>
-                </SheetHeader>
-                </SheetContent>
+                    <SheetHeader>
+                        <SheetTitle>Navegação do Mural</SheetTitle>
+                        <SheetDescription>Selecione uma das opções abaixo</SheetDescription>
+                    </SheetHeader>
 
+                    <nav 
+                        className="
+                            flex flex-col 
+                            gap-1
+                            px-4
+                            mb-4">
+                        <Link
+                            to="/"
+                            className="
+                                pl-2
+                                py-1
+                                text-lg
+                                hover:bg-blue-200 rounded-sm
+                                sm:text-2xl"
+                        >
+                            Home
+                        </Link>  
+
+                        <Link
+                            to="/"
+                            className="
+                                pl-2
+                                py-1
+                                text-lg
+                                hover:bg-blue-200 rounded-sm
+                                sm:text-2xl"
+                        >
+                            Meus avisos
+                        </Link>
+
+                        <Link
+                            to="/"
+                            className="
+                                pl-2
+                                py-1
+                                text-lg
+                                hover:bg-blue-200 rounded-sm
+                                sm:text-2xl"
+                        >
+                            Meu perfil
+                        </Link>  
+                    </nav>
+                </SheetContent>
             </Sheet>
         </header>
     )
--- FrontEnd/src/pages/home.tsx
+++ FrontEnd/src/pages/home.tsx
@@ -1,10 +1,109 @@
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
 
 function Home() {
-    return (
+  return (
+    <div 
+        className="
+            max-w-6xl mx-auto p-6"
+    >
         <div>
-            
+            <input 
+                type="text" 
+                placeholder="Buscar avisos..."
+                className="
+                    w-full 
+                    p-2
+                    border-2 rounded-sm"
+            />
         </div>
-    )
+
+        <div 
+            className="
+                flex gap-3
+                my-6
+                justify-center
+                items-center"
+        >
+            <span 
+                className="
+                    bg-blue-100 text-blue-600 
+                    px-3 py-0.5 rounded-2xl 
+                    cursor-pointer
+                    font-medium
+                    hover:bg-blue-200"
+            >
+                        Todos
+            </span>
+            <span
+                className="
+                bg-gray-200 text-gray-700
+                px-3 py-0.5 rounded-2xl
+                cursor-pointer
+                font-medium
+                hover:bg-red-200 hover:text-red-500"
+            >
+                Urgentes
+            </span>
+        </div>
+
+        <div 
+            className="
+                grid grid-cols-1
+                gap-6
+                sm:grid-cols-2"
+        >
+            <Card>
+                <CardHeader>
+                    <div className="
+                        flex justify-between mb-1
+                        text-sm"
+                    >
+                        <p>EVENTO</p>
+                        <p>18 Mar</p>
+                    </div>
+                    <CardTitle 
+                        className="font-bold text-lg"
+                    >
+                        Exposição de Artes 2026
+                    </CardTitle>
+                    <CardDescription>
+                        Preparem suas telas! A nossa
+                        exposição anual acontecerá no 
+                        pátio central...
+                    </CardDescription>
+                </CardHeader>
+                <CardFooter className="text-blue-400 font-medium">
+                    <a href="/">Saiba mais...</a>
+                </CardFooter>
+            </Card>
+
+            <Card>
+                <CardHeader>
+                    <div className="
+                        flex justify-between mb-1
+                        text-sm"
+                    >
+                        <p>EVENTO</p>
+                        <p>18 Mar</p>
+                    </div>
+                    <CardTitle 
+                        className="font-bold text-lg"
+                    >
+                        Exposição de Artes 2026
+                    </CardTitle>
+                    <CardDescription>
+                        Preparem suas telas! A nossa
+                        exposição anual acontecerá no 
+                        pátio central...
+                    </CardDescription>
+                </CardHeader>
+                <CardFooter className="text-blue-400 font-medium">
+                    <a href="/">Saiba mais...</a>
+                </CardFooter>
+            </Card>
+        </div>
+    </div>
+  )
 }
 
 export default Home;
