206754b74c71620c5e0357b6c7f31c6e42855344
IgorVinicius01 <igor.domingos@dcx.ufpb.br>
2026-03-18 01:28:54 -0300

    feate: componentização do Header e criação das rotas

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,46 +1,16 @@
-import { MenuIcon } from "lucide-react";
-import { Button } from "./components/ui/button";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./components/ui/sheet"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/home";
 
 function App() {
 
   return (
-   <div>
-    <header
-      className="
-        flex w-full h-25
-        justify-between
-        items-center
-        px-4
-        bg-blue-400"
-    >
-      <h1
-        className="
-          text-3xl
-          font-bold
-          text-white"  
-      >
-        Mural
-      </h1>
-
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button className="p-3 cursor-pointer"> 
-            <MenuIcon size={24}/> 
-          </Button>
-        </SheetTrigger>
-
-        <SheetContent side="top">
-          <SheetHeader>
-            <SheetTitle>Navegação do Mural</SheetTitle>
-            <SheetDescription>Selecione uma das opções abaixo</SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-
-      </Sheet>
-    </header>
-   </div>
-      
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
